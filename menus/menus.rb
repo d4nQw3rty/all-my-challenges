9755181a8059a8f9727ab8c4b08ea33285fdb846
Danys Linares <danlinmoy@gmail.com>
2024-02-24 14:54:10 -0400

    Add select options to menu_1 to navigate trough options

--- menus/menus.rb
+++ menus/menus.rb
@@ -7,10 +7,19 @@ class Menus
   end
   attr_reader :options_menu_1
   def menu_1
+    Styles.clear
     Styles.header
     Styles.options(options_menu_1) 
     Styles.ltext(" 0 -> Exit")
     Styles.ltext(" N -> Next Menu")
     Styles.footer
+    select = gets
+    if select.eql?("1")
+      Styles.clear
+      puts "Here comes the hello app"
+    else
+      Styles.clear
+      menu_1
+    end
   end
 end

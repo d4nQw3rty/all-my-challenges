37dd0f2a743d0080c8df03da9ed9a7ed4ae46bae
Danys Linares <danlinmoy@gmail.com>
2024-02-24 10:03:23 -0400

    Change options_menu_1 variable string value to and array. This will allow make an iteration over the arry to print all options

--- menus/menus.rb
+++ menus/menus.rb
@@ -1,9 +1,11 @@
 class Menus
   def initialize
-    @options_menu_1 = "hello, World"
+    @options_menu_1 = ["Hello World","Lasagna"]
   end
   attr_reader :options_menu_1
   def menu_1
-    puts options_menu_1
+    options_menu_1.each_with_index do |option, index|
+      puts "#{index+1} -> " + "#{option}" 
+    end
   end
 end

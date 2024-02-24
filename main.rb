5f2efa9bcca3dbc548de5d327ceca9450b6d1971
Danys Linares <danlinmoy@gmail.com>
2024-02-24 11:45:41 -0400

    Create a method clear This will make easier to clear the screen when needed instead of writing system("clear") you only have to call the clear function

--- main.rb
+++ main.rb
@@ -7,16 +7,20 @@ class App
     puts "WELCOME"
     stop = gets.chomp.to_i
     if stop.eql?(1)
+      clear
       menu.menu_1
       back = gets
       display
     elsif stop.eql?(2)
+      clear
       puts "Good bye"
     else
       display
     end
   end
-  display
+  def clear
+    system("clear")
+  end
 end
 
 app = App.new

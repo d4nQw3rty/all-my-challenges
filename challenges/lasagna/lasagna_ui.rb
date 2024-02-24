0d3c8479059b6262f2c90db1727a19dc3d6c1009
Danys Linares <danlinmoy@gmail.com>
2024-02-24 18:00:06 -0400

    Set lasagna in correct folder

---
+++ challenges/lasagna/lasagna_ui.rb
@@ -0,0 +1,118 @@
+require_relative '../styles/styles.rb'
+require_relative 'lasagna.rb'
+
+class LasagnaUi < Lasagna 
+ 
+ include Styles
+
+  OPTIONS = [
+    "Remaining minutes in oven",
+    "Preparation Time",
+    "Total Time",
+    "Main Menu"
+    ]
+  
+  def initialize
+    @layers = 0
+    @time_in_oven = 0
+  end
+
+  attr_reader :time_in_oven, :layer
+
+  def lasagna_menu 
+    system("clear")
+    Styles.header
+    Styles.title("Lest Prepare Lasagna!")
+    Styles.separation
+    Styles.title( "OPTIONS")
+    Styles.options(OPTIONS)   
+    Styles.separation
+    select = gets.chop.to_i
+    if select == 1
+      remaining
+    elsif select == 2
+      preparation
+    elsif select == 3
+      total_time
+    else
+      puts "return"
+    end
+
+  end
+
+  def remaining
+    system("clear")
+    Styles.header
+    Styles.title("How long the Lasagna has been in the oven?")
+    Styles.separation
+    time_in_oven = gets.chop.to_i
+    if time_in_oven.negative? 
+      Styles.header
+      Styles.title("Put it in the oven!")
+      Styles.separation
+      back = gets
+      lasagna_menu
+    elsif time_in_oven <= 40 && time_in_oven.positive?
+      Styles.header
+      Styles.title("You have to wait #{remaining_minutes_in_oven(time_in_oven)} minutes")
+      Styles.separation
+      back = gets
+      lasagna_menu
+    else
+      Styles.header
+      Styles.title("TAKE IT OUT OF THE OVEN!")
+      Styles.separation
+      back = gets
+      lasagna_menu
+    end
+  end
+
+  def preparation
+    system("clear")
+    Styles.header
+    Styles.title("Calculate your preparation time!")
+    Styles.separation
+    puts "Enter number of layers"
+    layers = gets.chop.to_i
+    if layers > 0
+      Styles.header
+      Styles.title("The preparation time is #{preparation_time_in_minutes(layers)} minutes")
+      Styles.separation
+      back = gets
+      lasagna_menu
+    else
+      Styles.header
+      Styles.title("Enter a valid value")
+      Styles.separation
+      back = gets
+      lasagna_menu
+    end
+  end
+
+  def total_time
+    system("clear")
+    Styles.header
+    Styles.title("How long time preparing a lasagna?")
+    Styles.separation
+    puts "Enter quantity of layers"
+    layers = gets.chop.to_i
+    puts "Enter current time in oven"
+    time_in_oven = gets.chop.to_i
+    if layers.eql?(0) || time_in_oven.eql?(0) || time_in_oven.negative? || layers.negative? || time_in_oven > 40 || layers > 20
+      Styles.header
+      Styles.title("Some values are invalid")
+      Styles.separation
+      back = gets
+      total_time
+    else
+      Styles.header
+      Styles.title("Cooking time is #{total_time_in_minutes(number_of_layers: layers, actual_minutes_in_oven: time_in_oven)} minutes by now...")
+      Styles.separation
+      back = gets
+      lasagna_menu
+    end
+  end
+end
+
+lasagna = Lasagna.new
+

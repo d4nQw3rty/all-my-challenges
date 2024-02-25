5db3777ec9af52bb390d85888343364820f5d2b5
Danys Linares <danlinmoy@gmail.com>
2024-02-25 17:01:13 -0400

    Create Amusement park UI, and part of the menu

--- UI/amusement_park_ui.rb
+++ UI/amusement_park_ui.rb
@@ -1,4 +1,39 @@
-require_relative "../challenges/amusement_park/amusement_park.rb"
-class AusementParkUi
+require_relative "../challenges/amusement-park/attendee.rb"
+require_relative "../styles/styles.rb" 
 
+class AmusementParkUi
+  include Styles
+
+  OPTIONS = [
+    "Height",
+    "Pass ID",
+    "Issue Pass",
+    "Revoke Pass"
+  ]
+
+  def amusement_menu
+    Styles.clear
+    Styles.header
+    Styles.title("Amusement Park")
+    Styles.footer
+    Styles.title("Lets create an atendee!")
+    Styles.title("Enter attendee height in centimeter")
+    Styles.footer
+    select = gets.chop.to_i
+    if  select > 0
+      attendee = Attendee.new(select)
+      Styles.clear
+      Styles.header
+      Styles.title("Amusement Park")
+      Styles.footer
+      Styles.header
+      Styles.ltext("Attendee create")
+      Styles.footer
+      attendee_stop = gets
+      amusement_menu
+    else
+      puts "Insert a valid value, example: 100 #{}"
+      error = gets
+    end
+  end
 end

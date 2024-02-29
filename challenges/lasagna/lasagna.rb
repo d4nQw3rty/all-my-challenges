c2b9f298d54b436ae29468266fc9b040305bda38
Danys Linares <danlinmoy@gmail.com>
2024-02-29 09:58:41 -0400

    Fix linters

--- challenges/lasagna/lasagna.rb
+++ challenges/lasagna/lasagna.rb
@@ -1,15 +1,16 @@
+# class Lasagna
 class Lasagna
-  EXPECTED_MINUTES_IN_OVEN  = 40
+  EXPECTED_MINUTES_IN_OVEN = 40
 
   def remaining_minutes_in_oven(actual_minutes_in_oven)
     EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    return layers*2
+    layers * 2
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-    2*number_of_layers + actual_minutes_in_oven
+    2 * number_of_layers + actual_minutes_in_oven
   end
 end

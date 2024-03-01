87c0986722d5a820d7d923b2781292d0cc3d5067
Danys Linares <danlinmoy@gmail.com>
2024-03-01 18:17:47 -0400

    Refactor amusement park Fix Linters

--- UI/amusement_park_ui.rb
+++ UI/amusement_park_ui.rb
@@ -1,107 +1,118 @@
-require_relative "../challenges/amusement-park/attendee.rb"
-require_relative "../styles/styles.rb" 
-
+require_relative '../challenges/amusement-park/attendee'
+require_relative '../styles/styles'
+# class AmusementParkUi
 class AmusementParkUi
   include Styles
 
-  OPTIONS = {
-    amp: ["Create Attendee", "Main Menu"],
-    attendee: ["Height", "Pass ID", "Issue Pass", "Revoke Pass","Go back"]
-  }
+  OPTIONS = { amp: ['Create Attendee'], attendee: ['Height', 'Pass ID', 'Issue Pass!', 'Revoke Pass!'] }
+
+  def amp_header
+    Styles.clear
+    Styles.htitle('Amusement Park')
+    Styles.title('Options')
+    Styles.line
+    Styles.options(OPTIONS[:amp])
+    Styles.ltext('0 -> Manin menu')
+    Styles.footer
+  end
 
   def amusementpark_menu
     amp_header
-    Styles.title("OPTIONS")
-    Styles.options(OPTIONS[:amp])
-    Styles.footer
     amp_option = gets.chomp.downcase
-    case amp_option
-    when "1"
+    if amp_option == '1'
       create_attendee
-    when "2"
-      puts "You Exit"
+    elsif amp_option == '0'
+      puts ''
     else
-      Styles.clear
       amusementpark_menu
     end
   end
 
   def create_attendee
-    Styles.clear
     amp_header
-    Styles.htitle("Lets create an atendee!","Enter attendee's height in centimeters")
-    attendee_height = gets.to_i
-    if  attendee_height > 0
-      attendee = Attendee.new(attendee_height)
-      attendee_menu(attendee)
+    Styles.htitle("Enter attendee's height in centimeters")
+    attendee_height = gets.chomp.to_i
+    if attendee_height.positive?
+      attendee_creator(attendee_height)
     else
-      puts "Insert a valid value, example: 100"
-      error = gets
+      puts 'Insert a valid value, example: 100'
+      back = gets
+      create_attendee if back
     end
+  end
+
+  def attendee_creator(attendee_height)
+    attendee = Attendee.new(attendee_height)
+    attendee_menu(attendee)
   end
 
   def attendee_menu(attendee)
     attendee_menu_header
-    check_option = gets.chomp.downcase
-    case check_option
-    when "5"
-      Styles.clear
-      amusementpark_menu
-    when "1"
-      attendee_menu_header
-      attendee_message("Attendee's Height is: #{attendee.height}")
-      check_options = gets
-      attendee_menu(attendee)
-    when "2"
-      attendee_menu_header
-      if attendee.pass_id.nil?
-        attendee_message("Pass ID: nil ") 
-      else
-        attendee_message("Pass ID: #{attendee.pass_id} ") 
-      end
-      check_options = gets
-      attendee_menu(attendee)
-    when "3"
-      attendee_menu_header
-      if attendee.pass_id.nil?
-        attendee.issue_pass!(rand(1..1000))
-        attendee_message("Now you have a pass ID: #{attendee.pass_id}") 
-      else
-        attendee_message("You already have a Pass ID: #{attendee.pass_id}")
-      end
-      check_option = gets
-      attendee_menu(attendee)
-    when "4"
-      attendee_menu_header
-      if attendee.pass_id.nil?
-        attendee_message("You don't have a pass!") 
-      else
-        attendee.revoke_pass!
-        attendee_message("Yous pass was revoked!") 
-      end
-        check_option = gets
-        attendee_menu(attendee)
+    select = gets.chomp.downcase
+    if %w[1 2].include?(select)
+      method_constructor(attendee, select)
+    elsif %w[0 3 4].include?(select)
+      issue_and_revoke(select, attendee)
     else
       attendee_menu(attendee)
     end
   end
 
-  def  attendee_menu_header
+  def method_constructor(attendee, select)
+    attendee_menu_header
+    opt = OPTIONS[:attendee][select.to_i - 1]
+    attendee_message("Attendee's #{opt} #{attendee.send opt.downcase.gsub(' ', '_')}")
+    attendee_back(attendee)
+  end
+
+  def issue_and_revoke(select, attendee)
+    attendee_menu_header
+    if select == '0'
+      puts ''
+    elsif select == '3'
+      issue(attendee)
+    else
+      revoke(attendee)
+    end
+  end
+
+  def attendee_menu_header
     Styles.clear
-    amp_header
-    Styles.title("OPTIONS")
+    Styles.htitle('Amusement Park')
+    Styles.title('Options')
+    Styles.line
     Styles.options(OPTIONS[:attendee])
+    Styles.ltext('0 -> Amusement Park Menu')
     Styles.footer
   end
-  
+
   def attendee_message(message)
     Styles.line
     Styles.ltext(message)
     Styles.footer
   end
 
-  def amp_header
-    Styles.clear
-    Styles.htitle("Amusement Park")
+  def issue(attendee)
+    if attendee.pass_id.nil?
+      attendee_message("Now you have a pass ID: #{attendee.issue_pass!(rand(1..1000))}")
+    else
+      attendee_message("You already have a Pass ID: #{attendee.pass_id}")
+    end
+    attendee_back(attendee)
+  end
+
+  def revoke(attendee)
+    if attendee.pass_id.nil?
+      attendee_message("You don't have a pass!")
+    else
+      attendee.revoke_pass!
+      attendee_message('Yous pass was revoked!')
+    end
+    attendee_back(attendee)
+  end
+
+  def attendee_back(attendee)
+    back = gets
+    attendee_menu(attendee) if back
   end
 end

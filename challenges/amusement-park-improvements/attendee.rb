bc5086e567f81474109d1f4b01709cb3bb8c6d94
Danys Linares <danlinmoy@gmail.com>
2024-03-04 17:13:36 -0400

    Refactor attendee_improvements.rb Add new method in UI to operate fit_ride? and allowed_to_ride? methods

--- challenges/amusement-park-improvements/attendee.rb
+++ challenges/amusement-park-improvements/attendee.rb
@@ -1,26 +1,32 @@
+# class Attendee
 class Attendee
   def issue_pass!(pass_id)
     @pass_id = pass_id
   end
 
   def revoke_pass!
-    @pass_id = nil
+    @pass_id = false
   end
 
   def has_pass?
-    @pass_id != nil
+    if @pass_id
+      true
+    else
+      false
+    end
   end
 
   def fits_ride?(ride_minimum_height)
-    @height >= ride_minimum_height
+    @attendee_height >= ride_minimum_height
   end
 
   def allowed_to_ride?(ride_minimum_height)
-    self.has_pass? && self.fits_ride?(ride_minimum_height)
+    has_pass? && fits_ride?(ride_minimum_height)
   end
 
   private
+
   def initialize(height)
-    @height = height
+    @attendee_height = height
   end
 end
